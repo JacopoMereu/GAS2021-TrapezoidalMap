79fc58ee24edefa920f0a47104e509c59bea8b5b
JacopoMereu <jachi334@gmail.com>
2022-04-03 17:45:11 +0200

    Added the function for querying the trapezoid containing a given point inside the dag

--- GAS_2122_TrapezoidalMaps/data_structures/dag.cpp
+++ GAS_2122_TrapezoidalMaps/data_structures/dag.cpp
@@ -9,6 +9,19 @@ DAG::DAG(Trapezoid* B)
     root = newNode(nodeType.trapezoid, B);
 }
 
+
+
+
+DAG::replaceNodeWithSubtree() {
+
+}
+
+
+Trapezoid& DAG::query(Point2& q) {
+    return queryRec(q, this->root);
+}
+
+// PRIVATE SECTION
 DAG::newNode(nodeType type, info info) {
         Node* new_node = (Node*)malloc(sizeof(Node));
         assert(new_node != nullptr);
@@ -22,6 +35,56 @@ DAG::newNode(nodeType type, info info) {
 }
 
 
-DAG::replaceNodeWithSubtree() {
+Trapezoid& DAG::queryRec(cg3::Point2& q, Node* root) {
+    switch (root->type) {
+    // x-node
+    case point:
+        // q.x < root.x => go left
+        if(q.x() < root->value.p->x()) {
+            return queryRec(q, root->lc);
+        }
+        // q.x > root.x => go right
+        else if (q.x() > root->value.p->x()) {
+            return queryRec(q, root->rc);
+        }
+        //TODO Should I handle q.x == root.x =?
+        break;
 
+    // y-node
+    case segment:
+        Position pos = getPointPositionRespectToLine(q, root->value.s);
+        switch (pos) {
+
+        // q above segment => go left
+        case left:
+            return queryRec(q, root->lc);
+            break;
+
+        // q below segment => go right
+        case right:
+            return queryRec(q, root->rc);
+            break;
+
+        //TODO Should I handle q on the segment =?
+        /*case center:
+            break;*/
+        default:
+            assert(false);
+            break;
+        }
+
+        break;
+
+    // leaf
+    case trapezoid:
+        // if we reach a leaf, the point is contained in the trapezoid associated to the node
+        return this->info.t;
+        break;
+
+    // shpuld be impossible
+    default:
+        assert(false);
+        break;
+    }
 }
+// END PRIVATE SECTION
--- GAS_2122_TrapezoidalMaps/data_structures/dag.h
+++ GAS_2122_TrapezoidalMaps/data_structures/dag.h
@@ -2,9 +2,12 @@
 #define DAG_H
 
 #include "cg3/geometry/point2.h"
-#include "cg3/geometry/segment2.h"
+#include "orderedsegment.h"
+//#include "cg3/geometry/segment2.h"
 //#include "cg3/geometry/polygon2.h"
 #include "trapezoid.h"
+#include "algorithms/OrientationUtility.h"
+
 
 class DAG
 {
@@ -16,13 +19,13 @@ public:
     enum nodeType {point, segment, trapezoid};
 
     void replaceNodeWithSubtree();
-
+    Trapezoid& query(Point2& q);
 private:
     // Content of a node
     union info {
         cg3::Point2* p;
-        cg3::Segment2* s;
-        Trapezoid* t; //TODO Do I need a trapezoid class???
+        OrderedSegment* s;
+        Trapezoid* t;
     };
 
     // Generic node structure
@@ -38,6 +41,7 @@ private:
 
     // Creates a new generic node
     Node* newNode(nodeType type, info info);
+    Trapezoid& queryRec(cg3::Point2& q, Node* root);
 };
 
 #endif // DAG_H
